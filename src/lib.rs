05796fcb1c98ca7cf78a0346b4e8bdffec48afae
HALKA9000-NIIP <csta19097@g.nihon-u.ac.jp>
2021-06-22 11:26:53 +0900

    man: os kernel init

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+#![no_std]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+#[no_mangle]
+pub extern "C" fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern fn kernel_main() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    let hello: &[u8] = b"SawAyaKanaAsa";
+    for (i, &byte) in hello.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0x0b;
+        }
+    }
+
+    loop {}
+}
+
